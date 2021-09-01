cb53317b4b6851f72d33456ae7f08034033144b3
cledson-leite <csbetsonline@gmail.com>
2021-09-01 18:51:41 -0300

    test: ensure the remote calls with the correct body

--- src/data/repositories/mocks/ApiPostClientSpy.ts
+++ src/data/repositories/mocks/ApiPostClientSpy.ts
@@ -1,9 +1,12 @@
-import { ApiPostClient } from '@datasource/ApiPostClient';
+import { ApiPostClient, ApiPostClientParams } from '@datasource/ApiPostClient';
+import { AuthenticationParams } from '@usecases/Authentication';
 
 export class ApiPostClientSpy implements ApiPostClient {
   public url?: string;
-  async post(url: string): Promise<void> {
-    this.url = url
+  public body?: AuthenticationParams
+  async post(params: ApiPostClientParams): Promise<void> {
+    this.url = params.url
+    this.body = params.body
     return Promise.resolve();
   }
 }
--- src/data/repositories/remoteAuthentication/RemoteAuthentication.spec.ts
+++ src/data/repositories/remoteAuthentication/RemoteAuthentication.spec.ts
@@ -1,5 +1,6 @@
 import { RemoteAuthentication } from '@repositories/remoteAuthentication/RemoteAuthentication'
 import { ApiPostClientSpy } from '@repositories/mocks/ApiPostClientSpy'
+import { fakerParams } from '@repositories/mocks/fakeParams'
 
 import faker from 'faker'
 
@@ -12,17 +13,29 @@ const makeSut = (url: string = faker.internet.url()): TypeSut => {
   const apiPostClient = new ApiPostClientSpy()
   const sut = new RemoteAuthentication(url, apiPostClient)
 
-  return {sut, api:apiPostClient}
+  return { sut, api: apiPostClient }
 }
 
 describe('Remote Authentication', () => {
   it('Should call the ApiPostClient with the correct url', () => {
     //produz os dados do teste
     const url = faker.internet.url()
-    const {sut, api} = makeSut(url)
+    const { sut, api } = makeSut(url)
+    const params = fakerParams()
     //operacional esses dados
-    sut.auth()
+    sut.auth(params)
     //verificar resultado esperado
     expect(api.url).toBe(url)
+  })
+
+  it('Should call the ApiPostClient with the correct body', () => {
+    //produz os dados do teste
+    const { sut, api } = makeSut()
+    const params = fakerParams()
+
+    //operacional esses dados
+    sut.auth(params)
+    //verificar resultado esperado
+    expect(api.body).toEqual(params)
   })
 })
