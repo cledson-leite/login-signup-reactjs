c0cf48de11eaa341bc07c909c114b7c74a3ff5b6
cledson-leite <csbetsonline@gmail.com>
2021-09-10 18:17:06 -0300

    test: ensure RemoteAuthentication throws UnexpectedError on default cases

--- src/data/repositories/mocks/ApiPostClientSpy.ts
+++ src/data/repositories/mocks/ApiPostClientSpy.ts
@@ -1,12 +1,17 @@
+import { ApiResponse, ApiStatusCode } from '@datasource/api/ApiResopnse';
 import { ApiPostClient, ApiPostClientParams } from '@datasource/ApiPostClient';
 import { AuthenticationParams } from '@usecases/Authentication';
 
 export class ApiPostClientSpy implements ApiPostClient {
-  public url?: string;
+  public url?: string
   public body?: AuthenticationParams
-  async post(params: ApiPostClientParams): Promise<void> {
+  public response: ApiResponse = {
+    statusCode: ApiStatusCode.ok
+  }
+
+  async post(params: ApiPostClientParams): Promise<ApiResponse> {
     this.url = params.url
     this.body = params.body
-    return Promise.resolve();
+    return Promise.resolve(this.response);
   }
 }
--- src/data/repositories/remoteAuthentication/RemoteAuthentication.spec.ts
+++ src/data/repositories/remoteAuthentication/RemoteAuthentication.spec.ts
@@ -1,8 +1,11 @@
+import { ApiStatusCode } from '@datasource/api/ApiResopnse'
 import { RemoteAuthentication } from '@repositories/remoteAuthentication/RemoteAuthentication'
 import { ApiPostClientSpy } from '@repositories/mocks/ApiPostClientSpy'
 import { fakerParams } from '@repositories/mocks/fakeParams'
+import { InvalidCredencialsError } from '@repositories/errors/InvalidCredencialsError'
 
 import faker from 'faker'
+import { UnexpectedError } from '@repositories/errors/UnexpectedError'
 
 type TypeSut = {
   sut: RemoteAuthentication,
@@ -37,5 +40,53 @@ describe('Remote Authentication', () => {
     sut.auth(params)
     //verificar resultado esperado
     expect(api.body).toEqual(params)
+  })
+
+  it('Should throw InvalidCredentialError if ApiPostClient returns 401', async () => {
+    //produz os dados do teste
+    const { sut, api } = makeSut()
+    api.response = {
+      statusCode: ApiStatusCode.unauthorized
+    }
+    //operacionar esses dados
+    const promise = sut.auth(fakerParams())
+    //verificar resultado esperado
+    await expect(promise).rejects.toThrow(new InvalidCredencialsError())
+  })
+
+  it('Should throw UnexpectedError if ApiPostClient returns 400', async () => {
+    //produz os dados do teste
+    const { sut, api } = makeSut()
+    api.response = {
+      statusCode: ApiStatusCode.BadRequest
+    }
+    //operacionar esses dados
+    const promise = sut.auth(fakerParams())
+    //verificar resultado esperado
+    await expect(promise).rejects.toThrow(new UnexpectedError())
+  })
+
+  it('Should throw UnexpectedError if ApiPostClient returns 404', async () => {
+    //produz os dados do teste
+    const { sut, api } = makeSut()
+    api.response = {
+      statusCode: ApiStatusCode.notFound
+    }
+    //operacionar esses dados
+    const promise = sut.auth(fakerParams())
+    //verificar resultado esperado
+    await expect(promise).rejects.toThrow(new UnexpectedError())
+  })
+
+  it('Should throw UnexpectedError if ApiPostClient returns 500', async () => {
+    //produz os dados do teste
+    const { sut, api } = makeSut()
+    api.response = {
+      statusCode: ApiStatusCode.serverError
+    }
+    //operacionar esses dados
+    const promise = sut.auth(fakerParams())
+    //verificar resultado esperado
+    await expect(promise).rejects.toThrow(new UnexpectedError())
   })
 })
