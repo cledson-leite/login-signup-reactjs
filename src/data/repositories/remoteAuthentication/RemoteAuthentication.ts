ddcbba7ab71c84df750fa6824e464e90b7385bf7
cledson-leite <csbetsonline@gmail.com>
2021-09-10 20:38:27 -0300

    feat: ensure RemoteAuthentication returns an Account on success

--- src/data/repositories/remoteAuthentication/RemoteAuthentication.ts
+++ src/data/repositories/remoteAuthentication/RemoteAuthentication.ts
@@ -1,22 +1,22 @@
-import { AuthenticationParams } from '@usecases/Authentication';
+import { Authentication, AuthenticationParams } from '@usecases/Authentication';
 import { ApiPostClient } from '@datasource/ApiPostClient';
 import { ApiStatusCode } from '@datasource/api/ApiResponse';
 import { UnexpectedError } from '@repositories/errors/UnexpectedError';
 import { Account } from '@entities/Account';
 import { InvalidCredentialsError } from '@repositories/errors/InvalidCredentialsError';
 
-export class RemoteAuthentication {
+export class RemoteAuthentication implements Authentication {
   constructor(
     private readonly url: string,
     private readonly api: ApiPostClient<AuthenticationParams, Account>
   ) { };
-  async auth(params: AuthenticationParams): Promise<void> {
+  async auth(params: AuthenticationParams): Promise<Account> {
     const apiResponse = await this.api.post({
       url: this.url,
       body: params
     })
     switch (apiResponse.statusCode) {
-      case ApiStatusCode.ok: break
+      case ApiStatusCode.ok: return apiResponse.body!
       case ApiStatusCode.unauthorized: throw new InvalidCredentialsError()
       default: throw new UnexpectedError()
     }
