67f0891970ab1dee0a7c4e8ee3bfdf2d6d887a46
cledson-leite <csbetsonline@gmail.com>
2021-09-11 11:40:28 -0300

    feat: ensure AxiosClient calls axios with correct body

--- src/service/Axios/AxiosClient.ts
+++ src/service/Axios/AxiosClient.ts
@@ -4,6 +4,6 @@ import { ApiPostClientParams } from '@datasource/ApiPostClient';
 
 export class AxiosClient {
   async post(params: ApiPostClientParams<any>): Promise<void>{
-    await axios.post(params.url)
+    await axios.post(params.url, params.body)
   }
 }
