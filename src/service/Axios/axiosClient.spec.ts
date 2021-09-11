8d483d7b420042e3402de9a51e553a5169b8ad9d
cledson-leite <csbetsonline@gmail.com>
2021-09-11 11:16:06 -0300

    test: ensure AxiosClient calls axios with correct URL

---
+++ src/service/Axios/axiosClient.spec.ts
@@ -0,0 +1,23 @@
+import faker from 'faker'
+import axios from 'axios'
+
+import { AxiosClient } from '@service/Axios/AxiosClient'
+
+jest.mock('axios')
+const mockedAxios = axios as jest.Mocked<typeof axios>
+
+const makeSut = (): AxiosClient => {
+  return new AxiosClient()
+}
+
+describe('Axios Client', () => {
+  it('Should call axios with the correct URL verb and body', () => {
+    //produz os dados do teste
+    const url = faker.internet.url()
+    const sut = makeSut()
+    //operacionar esses dados
+    sut.post({ url: url})
+    //verificar resultado esperado
+    expect(mockedAxios).toHaveBeenCalledWith(url)
+  })
+})
