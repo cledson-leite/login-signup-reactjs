c5284878676b1891cb4d7179a65cd92832e25915
cledson-leite <csbetsonline@gmail.com>
2021-10-15 12:58:21 -0300

    test: ensure Login start with inputs with required state

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -5,13 +5,21 @@ import Login from './login'
 describe('Login', () => {
   it('Should start with inicial state', () => {
     //produz os dados do teste
-    const { getByTestId } = render(<Login />)
+    const { getByTestId, queryByTestId } = render(<Login />)
     //operacionar esses dados
     const errorWrap = getByTestId('errorWrap')
     const submitButton = getByTestId('submit') as HTMLButtonElement
+    const emailStatus = getByTestId('email-status')
+    const iconEmail = emailStatus.firstElementChild?.getAttribute('data-icon')
+    const passwordStatus = getByTestId('password-status')
+    const iconPassword = passwordStatus.firstElementChild?.getAttribute('data-icon')
     
     //verificar resultado esperado
     expect(errorWrap.childElementCount).toBe(0)
     expect(submitButton.disabled).toBe(true)
+    expect(emailStatus?.title).toBe('Campo Obrigatório')
+    expect(iconEmail).toBe('check')
+    expect(passwordStatus?.title).toBe('Campo Obrigatório')
+    expect(iconPassword).toBe('check')
   })
 })
