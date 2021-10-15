45d689b5a1a887a5f6246262259270fc59150d62
cledson-leite <csbetsonline@gmail.com>
2021-10-15 13:13:50 -0300

    refactor: remove autofill input on chrome

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -10,17 +10,20 @@ type Props = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>
 
 const Input: React.FC<Props> = (props: Props) => {
   const { inputError } = useContext(Context)
+  const enabledInput = (e: React.FocusEvent<HTMLInputElement>) => {
+    e.target.readOnly = false;
+  }
   return (
     <div className={Styles.inputWrap}>
-      <input {...props} />
+      <input {...props} readOnly onFocus={ enabledInput }/>
       <span
         data-testid={`${props.name}-status`}
         title={inputError[props.name!]}
-        className={Styles.statusGreen}
+        className={Styles.statusRed}
       >
-        <FontAwesomeIcon icon={faCheck} />
+        <FontAwesomeIcon icon={faTimes} />
       </span>
-      {/* <span className={Styles.statusRed}><FontAwesomeIcon icon={faTimes} /></span> */}
+      {/* <span className={Styles.statusRed}><FontAwesomeIcon icon={faCheck} /></span> */}
     </div>
   )
 }
