45b097cf554acf136457b813d37390e3ee10f34e
cledson-leite <csbetsonline@gmail.com>
2021-10-25 12:07:35 -0300

    feat: ensure EmailValidation returns falsy if email is valid

--- src/presentation/validators/email/EmailValidation.ts
+++ src/presentation/validators/email/EmailValidation.ts
@@ -4,7 +4,8 @@ import { FieldValidation } from '../repositories/FieldValidation';
 export class EmailValidation implements FieldValidation {
   constructor(readonly field: string) { };
   validate(value: string): string {
-    return new InvalidFieldError().message
+    const emailRegex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+    return emailRegex.test(value) ? "" : new InvalidFieldError().message
   }
 
 }
