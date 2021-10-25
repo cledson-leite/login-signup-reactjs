50adf7b79b7181b6c93f7c55254f13b5d335d19b
cledson-leite <csbetsonline@gmail.com>
2021-10-25 11:23:18 -0300

    feat: ensure RequiredField returns falsy if field not is empty

--- src/presentation/validation/RequiredField.ts
+++ src/presentation/validation/RequiredField.ts
@@ -2,9 +2,9 @@ import { RequiredFieldError } from './errors/RequiredFieldError';
 import { FieldValidation } from './repositories/FieldValitation';
 
 export class RequiredField implements FieldValidation {
-  constructor(readonly field: string) { };
-  validate(value: string): Error {
-    return new RequiredFieldError()
+  constructor(readonly field: string){};
+  validate(value: string): string {
+    return value ? "" : new RequiredFieldError().message
   }
 
 }
