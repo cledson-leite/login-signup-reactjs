28cd1ccb9db1c576d8a59b386d4063e6245fd857
cledson-leite <csbetsonline@gmail.com>
2021-10-27 11:38:32 -0300

    test: ensure ValidationComposite returns the first error found

--- src/presentation/validators/validationComposite/composite.spec.ts
+++ src/presentation/validators/validationComposite/composite.spec.ts
@@ -5,8 +5,9 @@ describe('Validation Composite', () => {
   it('Should return error if any validation fails', () => {
     //produz os dados do teste
     const fieldValidationSpy = new FieldValidationSpy('any_field')
+    fieldValidationSpy.error = new Error('first_error_message')
     const fieldValidationSpy2 = new FieldValidationSpy('any_field')
-    fieldValidationSpy2.error = new Error('any_error_message')
+    fieldValidationSpy2.error = new Error('second_error_message')
     const sut = new ValidationComposite([
       fieldValidationSpy,
       fieldValidationSpy2
@@ -14,7 +15,7 @@ describe('Validation Composite', () => {
     //operacionar esses dados
     const error = sut.validate('any_field', 'any_value')
     //verificar resultado esperado
-    expect(error).toBe('any_error_message')
+    expect(error).toBe('first_error_message')
     
   })
 })
