7f1daf304e84b04e510629dadb96f5969c49721a
cledson-leite <csbetsonline@gmail.com>
2021-10-27 11:47:54 -0300

    refactor: move SUT creation to a factory method

--- src/presentation/validators/validationComposite/composite.spec.ts
+++ src/presentation/validators/validationComposite/composite.spec.ts
@@ -1,17 +1,30 @@
 import { ValidationComposite } from './Composite'
 import { FieldValidationSpy } from './mock/FieldValidationSpy'
 
+type SutTypes = {
+  sut: ValidationComposite,
+  fieldValidationsSpy: FieldValidationSpy[]
+}
+
+const makeSut = (): SutTypes => {
+  const fieldValidationsSpy = [
+    new FieldValidationSpy('any_field'),
+    new FieldValidationSpy('any_field')
+  ]
+  const sut = new ValidationComposite(fieldValidationsSpy)
+
+  return {
+    sut,
+    fieldValidationsSpy
+  }
+}
+
 describe('Validation Composite', () => {
   it('Should return error if any validation fails', () => {
     //produz os dados do teste
-    const fieldValidationSpy = new FieldValidationSpy('any_field')
-    fieldValidationSpy.error = new Error('first_error_message')
-    const fieldValidationSpy2 = new FieldValidationSpy('any_field')
-    fieldValidationSpy2.error = new Error('second_error_message')
-    const sut = new ValidationComposite([
-      fieldValidationSpy,
-      fieldValidationSpy2
-    ])
+    const { sut, fieldValidationsSpy } = makeSut()
+    fieldValidationsSpy[0].error = new Error('first_error_message')
+    fieldValidationsSpy[1].error = new Error('second_error_message')
     //operacionar esses dados
     const error = sut.validate('any_field', 'any_value')
     //verificar resultado esperado
