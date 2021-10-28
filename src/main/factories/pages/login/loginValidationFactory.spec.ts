76956fd0becb19e6e53fc308831a97b27fa7edf9
cledson-leite <csbetsonline@gmail.com>
2021-10-28 12:33:21 -0300

    test: ensure ValidationComposite users correct validations

---
+++ src/main/factories/pages/login/loginValidationFactory.spec.ts
@@ -0,0 +1,20 @@
+import { ValidationBuilder } from
+  '../../../../presentation/builder/validation/ValidationBuilder'
+import { ValidationComposite } from
+  '../../../../presentation/validators/validationComposite/Composite'
+import { makeLogin } from './loginFactory'
+import { makeLoginValidation } from './loginValidationFactory'
+
+describe('Login validation factory', () => {
+  it('Should make ValidationComposite with correct validation', () => {
+    //produz os dados do teste
+    const composite = makeLoginValidation()
+    //operacionar esses dados
+    const validationComposite = ValidationComposite.build([
+      ...ValidationBuilder.field('email').required().email().build(),
+      ...ValidationBuilder.field('password').required().min(5).build(),
+    ])
+    //verificar resultado esperado
+    expect(composite).toEqual(validationComposite)
+  })
+})
